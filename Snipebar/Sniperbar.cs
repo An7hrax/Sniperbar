0272b0c8f8b376331cb4f3dfa0839290a72e3fca
an7hrax <an7hrax@gmail.com>
2021-02-16 13:50:54 +0100

    Updated

--- Snipebar/Sniperbar.cs
+++ Snipebar/Sniperbar.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using ExileCore;
 using ExileCore.PoEMemory.Components;
 using ExileCore.Shared.Enums;
@@ -16,7 +12,7 @@ namespace Sniperbar
         {
             var buffs = GameController.EntityListWrapper.Player.GetComponent<Life>()
                                                                 .Buffs
-                                                                .ToList(); //cab use <Buffs>().ToList();
+                                                                .ToList();
             var scrRect = GameController.Window.GetWindowRectangle();
             var center = new Vector2(scrRect.X + scrRect.Width / 2, scrRect.Height - 500);
             center.Y -= 15;
@@ -31,21 +27,18 @@ namespace Sniperbar
             {
                 if (buffs.Exists(b => b.Name == "snipe_stacks" && b.Charges == 1))
                 {
-
                     Graphics.DrawBox(new RectangleF(scrRect.X - 2 - (castbar_part * 3) + scrRect.Width / 2, scrRect.Height - 500 - 2, castbar_w + 4, castbar_h + 4), Color.Black);
                     Graphics.DrawBox(new RectangleF(scrRect.X - (castbar_part * 3) + scrRect.Width / 2, scrRect.Height - 500, castbar_part * 1, castbar_h), Color.Yellow);
                     Graphics.DrawText("1", textRect, Color.Yellow, "consolab:13", FontAlign.Center);
                 }
                 if (buffs.Exists(b => b.Name == "snipe_stacks" && b.Charges == 2))
                 {
-
                     Graphics.DrawBox(new RectangleF(scrRect.X - 2 - (castbar_part * 3) + scrRect.Width / 2, scrRect.Height - 500 - 2, castbar_w + 4, castbar_h + 4), Color.Black);
                     Graphics.DrawBox(new RectangleF(scrRect.X - (castbar_part * 3) + scrRect.Width / 2, scrRect.Height - 500, castbar_part * 2, castbar_h), Color.Yellow);
                     Graphics.DrawText("2", textRect, Color.Yellow, "consolab:16", FontAlign.Center);
                 }
                 if (buffs.Exists(b => b.Name == "snipe_stacks" && b.Charges == 3))
                 {
-
                     Graphics.DrawBox(new RectangleF(scrRect.X - 2 - (castbar_part * 3) + scrRect.Width / 2, scrRect.Height - 500 - 2, castbar_w + 4, castbar_h + 4), Color.Black);
                     Graphics.DrawBox(new RectangleF(scrRect.X - (castbar_part * 3) + scrRect.Width / 2, scrRect.Height - 500, castbar_part * 3, castbar_h), Color.Orange);
                     Graphics.DrawText("3", textRect, Color.Orange, "consolab:18", FontAlign.Center);
